97375feebab4ec6c40a4b856a75509af8b5c095a
amirbek123321 <amirbekuldasev72@gmail.com>
2023-09-16 18:42:07 +0500

    js1

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log(100);
+
+
+var imena = prompt('введите имя');
+
+console.log(imena);
+
+var vozvrast = prompt('введите возвраст')
+
+console.log(vozvrast)
+
+
+var ans1 = prompt('22 + 3')
+
+console.log('22 + 3 = 25 ( ваш ответ ' + ans1 + ')')
+
+
+
+
+
+var ans2 = prompt('15 - 10')
+
+console.log('15 - 10 = 5 ( ваш ответ ' + ans2 + ')')
+
+
+
+
+
+var ans3 = prompt('20 * 10')
+
+console.log(' 20 * 10 = 200 ( ваш ответ ' + ans3 + ')')
+
+
+
+
+
+var ans4 = prompt('100 / 2')
+
+console.log('100 / 2 = 50 ( ваш ответ ' + ans4 + ')')
+
+
+
+
+
+var ans5 = prompt('12 % 5')
+
+console.log('12 % 5 = 2 (ваш ответ ' + ans5 + ')')
